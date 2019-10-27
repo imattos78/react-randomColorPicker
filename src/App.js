96917740aac7d0fdfa2e9f6988b786151f40e38d
ignacio <nachomattos78@gmail.com>
2019-10-27 17:10:12 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Button} from './Button';
+import './style.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      color: [22, 220, 100]
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+  // Lifecycle methods
+
+  componentDidMount() {
+    this.applyColor();
+  }
+
+  componentDidUpdate(prevProps, prevState) { 
+    this.applyColor();
+  }
+// Tranform numbers to rbg numbers
+  formatColor(ary) {
+    return 'rgb(' + ary.join(', ') + ')';
+  }
+// Change the color of the text
+  isLight() {
+    const rgb = this.state.color;
+    return rgb.reduce((a,b) => a+b) < 127 * 3;
+  }
+// Apply the background color 
+  applyColor() {
+    const color = this.formatColor(this.state.color);
+    document.body.style.background = color;
+  }
+// Create an array of 3 numbers from 0 to 256
+  chooseColor() {
+    const random = [];
+    for (let i = 0; i < 3; i++) {
+      random.push(Math.floor(Math.random()*256));
+    }
+    return random;
+  }
+  // Click event function
+  handleClick(){
+    this.setState({
+      color: this.chooseColor()
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1 className={this.isLight() ? 'white' : 'black'}>
+          Your color is {this.formatColor(this.state.color)}
+        </h1>
+        <Button light={this.isLight()} onClick={this.handleClick}/>
+      </div>
+    );
+  }
 }
+
+
 
 export default App;
